7a31f00da71b58991af1b33bf0f732b9d102fc2b
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-10-31 17:33:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 var express = require("express"); 
 const path = require('path');
 const cfenv = require('cfenv');
-const cfenv = require('cfenv');
-const cfenv = require('cfenv');
 
 var app  =  express(); 
 var appEnv = cfenv.getAppEnv();
