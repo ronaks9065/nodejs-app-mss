6cb6ed137f5767da8671be9153bbd6ad09f0b11f
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-11-01 11:17:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.get('/redirect', function(req, res) {
       })
 
  app.listen(app.get('port'), function() {
-        console.log("Node app is running at http://localhost:" + app.get('port') +"/mithuntechnologies");
+        console.log("Node JS app is running at http://localhost:" + app.get('port') +"/mithuntechnologies");
       })
     
 
